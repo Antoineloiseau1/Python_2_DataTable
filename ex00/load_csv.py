c7828e1e890ec8ea4d4e3e87c7e826087ac5e5d1
Antoine Loiseau <anloisea@c1r4p7.42nice.fr>
2023-03-20 15:24:37 +0100

    ex00

---
+++ ex00/load_csv.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+
+def load(path: str) -> pd.DataFrame:
+    """def load(path: str) -> pd.DataFrame:
+
+Opens a .csv file given by path, display its \
+dimensions and returns it as a pandas.DataFrame file
+"""
+    try:
+        if type(path) is not str:
+            raise TypeError("TypeError: file must be type .csv")
+        if (not path.endswith(".csv")):
+            raise Exception("ExtensionError: file must be type .csv")
+        file = pd.read_csv(path)
+        print("Loading dataset of dimensions:", file.shape)
+    except Exception as msg:
+        print(msg)
+        return None
+    return file
