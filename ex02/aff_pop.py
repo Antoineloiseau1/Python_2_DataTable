b66377f2e5ab0488ac149ac8e192a533b9680713
Antoine Loiseau <anloisea@c3r1p4.42nice.fr>
2023-03-21 16:52:23 +0100

    ex02

--- ex02/aff_pop.py
+++ ex02/aff_pop.py
@@ -11,33 +11,41 @@ This program displays the population by years of a selectd country
     file = load("population_total.csv")
 
     # Extracting Data from contries
-    france = file.loc[file['country'] == 'France']
-    belgium = file.loc[file['country'] == 'Belgium']
+    first = file.loc[file['country'] == 'France']
+    second = file.loc[file['country'] == 'Afghanistan']
 
     # setting x_plots for each contry
-    x_france = france.columns[1:252]
-    x_france = pd.to_numeric(x_france)
-    x_belgium = belgium.columns[1:252]
-    x_belgium = pd.to_numeric(x_belgium)
+    first_name = first.iloc[0][0]
+    second_name = second.iloc[0][0]
+    x_first = first.columns[1:252]
+    x_first = pd.to_numeric(x_first)
+    x_second = second.columns[1:252]
+    x_second = pd.to_numeric(x_second)
 
     # setting y_plots for each contry
-    exp = {'k': 1e3, 'm': 1e6}
-    y_france = france.iloc[0][1:252]
-    y_france = [int(float(i[:-1]) * exp[i[-1].lower()]) for i in y_france]
-    y_france = pd.DataFrame(y_france)
-    y_belgium = belgium.iloc[0][1:252]
-    y_belgium = [int(float(i[:-1]) * exp[i[-1].lower()]) for i in y_belgium]
-    y_belgium = pd.DataFrame(y_belgium)
+    exp = {'k': 1e3, 'm': 1e6, 'b': 1e9}
+    y_first = first.iloc[0][1:252]
+    y_first = [int(float(i[:-1]) * exp[i[-1].lower()]) for i in y_first]
+    y_first = pd.DataFrame(y_first)
+    y_second = second.iloc[0][1:252]
+    y_second = [int(float(i[:-1]) * exp[i[-1].lower()]) for i in y_second]
+    y_second = pd.DataFrame(y_second)
 
     # Setting ticks
     fig, ax = plt.subplots()
     plt.xticks(range(1800, 2051, 40))
-    ax.set_yticks([0, 20000000, 40000000, 60000000, 80000000])
-    ax.set_yticklabels(["0M", "20M", "40M", "60M", "80M"])
+    max_value = max(int(y_first.max()), int(y_second.max()))
+    if (max_value > 1e9):
+        step = 200000000
+    else:
+        step = 20000000
+    yticks = (range(0, max_value, step))
+    ylabels = [f"{i/1e6:.0f}M" for i in yticks]
+    plt.yticks(yticks, ylabels)
 
     # Setting the entire plot to show
-    ax.plot(x_belgium, y_belgium, label='Belgium')
-    ax.plot(x_france, y_france, color='green', label='France')
+    ax.plot(x_second, y_second, label=second_name)
+    ax.plot(x_first, y_first, color='green', label=first_name)
     ax.legend(loc='lower right')
     plt.title('Population Projections')
     plt.ylabel('Population')
